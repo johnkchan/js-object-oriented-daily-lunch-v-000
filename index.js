d88cc634a9a313f32a8d4917b196fb92e903afc1
Jonathan Chan <johnkchan252@gmail.com>
2019-06-12 04:07:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 0;
 let customerId = 0;
+let mealId = 0;
+let deliveryId = 0;
+
+class Neighborhood {
+  constructor(name){
+    this.id = ++neighborhoodID;
+    this.name = name;
+    
+    store.neighborhoods.push(this);
+  }
+}
