0fc7528b7b9530809d7d647d72900033fea07785
Jonathan Chan <johnkchan252@gmail.com>
2019-06-12 04:56:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -86,11 +86,11 @@ class Meal {
   }
   
   static byPrice() {
-      return store.meals.sort(
-        function (a, b) {
-          return b.price - a.price;
-        }
-      );
+    return store.meals.sort(
+      function (a, b) {
+        return b.price - a.price;
+      }
+    );
   }
 }
 
